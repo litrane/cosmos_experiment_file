5d716142e9cfe54a94da955f939a9d78854ad293
litarne <705290832@qq.com>
2022-11-07 15:37:00 +0800

    update

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+host_string=("'root@20.220.200.72'" "'root@20.163.215.150'" "'root@20.163.221.185'" "'root@20.231.77.191'")
+name="deploy-cosmos"
+for i in $( seq 0 ${#host_string[@]} )
+tmux_name="$name:$i"
+do
+  #tmux neww -a -n "$client" -t $name
+  if [ "$1" == "init" ]; then 
+  tmux new-session -s $name -d
+  tmux new-window -n "$i" -t "$name" -d
+  tmux send -t $tmux_name "ssh ${host_string[i]}" Enter
+  tmux send -t $tmux_name "git clone https://github.com/litrane/cosmos_experiment_file.git" Enter
+  tmux send -t $tmux_name "cd cosmos_experiment_file" Enter
+  tmux send -t $tmux_name "nohup ./earthd start --home=./workspace/earth/validator${i} > output 2>&1 & " Enter
+elif [ "$1" == "update" ]; then
+  #tmux send -t $tmux_name "cd theta_experiment_file" Enter
+  tmux send -t $tmux_name "git clean -xfd" Enter
+  tmux send -t $tmux_name "git pull" Enter
+elif [ "$1" == "clean" ]; then
+  tmux send -t $tmux_name "cd ~" Enter
+  tmux send -t $tmux_name "rm -rf cosmos_experiment_file" Enter
+fi
+  val=`expr $i + 1`
+  echo "start node${val}!"
+  
+
+done
+
